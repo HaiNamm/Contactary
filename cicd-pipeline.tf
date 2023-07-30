d77b18d4832abf7ab56e0dc5a3964bad1acc8d9d
HaiNamm <dhtn8xx@gmail.com>
2023-07-30 16:51:37 +0700

    new

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -1,5 +1,5 @@
 resource "aws_codebuild_project" "tf-plan" {
-  name          = "tf-cicd-plan2"
+  name          = "tf-cicd-plan"
   description   = "Plan stage for terraform"
   service_role  = aws_iam_role.tf-codebuild-role.arn
 
