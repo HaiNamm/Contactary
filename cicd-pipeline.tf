407716d2138d7d98956ccb9b004d20d5bab06dab
HaiNamm <dhtn8xx@gmail.com>
2023-07-30 16:37:55 +0700

    new

--- cicd-pipeline.tf
+++ cicd-pipeline.tf
@@ -56,7 +56,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
 
     artifact_store {
         type="S3"
-        location = aws_s3_bucket.aws-aosnoteterraformstate-bucket.id
+        location = aws_s3_bucket.aws-aosnoteterraformstate.id
     }
 
     stage {
