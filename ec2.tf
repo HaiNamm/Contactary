93068bf4d2f50cdac8fd57cfe85ff9116a7652d8
HaiNamm <dhtn8xx@gmail.com>
2023-07-31 21:16:42 +0700

    a

--- ec2.tf
+++ ec2.tf
@@ -71,8 +71,8 @@ resource "aws_instance" "ec2_instance" {
 
               # Tải mã nguồn của trang web React từ GitHub và triển khai nó
               sudo su ec2-user -c "cd /var/www/html && wget https://github.com/Contactary/contactary-fe/archive/refs/heads/develop.zip"
-              su do unzip develop.zip
-              sudo su ec2-user -c "cd /var/www/html/contactary-fe && npm install && npm run build"
+              sudo su ec2-user -c "cd /var/www/html && unzip develop.zip"
+              sudo su ec2-user -c "cd /var/www/html/contactary-fe-develop && npm install && npm run build"
 
               # Tạo tệp cấu hình Apache cho ứng dụng React
               sudo bash -c 'cat << EOT > /etc/httpd/conf.d/react_app.conf
