f502c1d8b53b1312ec367eadd5c99095b042f68d
HaiNamm <dhtn8xx@gmail.com>
2023-07-31 16:08:04 +0700

    new1

--- install_web.sh
+++ install_web.sh
@@ -6,6 +6,7 @@ cd /var/www/html
 wget https://github.com/Contactary/contactary-fe/archive/refs/heads/develop.zip
 unzip develop.zip
 cp -r contactary-fe-develop/* /var/www/html/
+mv /var/www/html/contactary-fe-develop/index.html /var/www/html/
 rm -rf contactary-fe-develop develop.zip
 systemctl enable httpd 
 systemctl start httpd
