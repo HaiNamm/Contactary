ec32cc00f01c88e7164dba5f87eebb365173df44
HaiNamm <86754745+HaiNamm@users.noreply.github.com>
2023-08-02 10:01:36 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { RouterProvider } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
 import { routes } from "./routes/routes";
 
 function App() {
   return (
-    <>
-        <RouterProvider router={routes} />
-    </>
+    <BrowserRouter>
+      <Switch>
+        {/* Thêm các Route tại đây */}
+        <Route path="/login" component={Login} />
+        {/* Các Route khác trong routes nếu có */}
+      </Switch>
+    </BrowserRouter>
   );
 }
 
